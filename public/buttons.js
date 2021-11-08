a35234d2a318daccf6c0024733837ecc867fa95d
Doug Franklin <82669390+CoderFranklin@users.noreply.github.com>
2021-11-08 12:54:17 -0600

    update

---
+++ public/buttons.js
@@ -0,0 +1,13 @@
+let button_left = document.getElementsByClassName('sliding-system')
+console.log(button_left);
+button_left.onclick = function(){
+let clicks = button_left;
+}
+
+if (clicks == 2){
+//button_left.onclick = function(){ //works
+//button_left.style.transform = "translate3d(-10%, 0, 0)";
+
+//}
+alert("Fuck")
+}
